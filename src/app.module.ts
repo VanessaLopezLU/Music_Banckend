e9e9e8824e327086d040f86a36a76333abd979ef
Vanessa <leylopez32@gmail.com>
2024-07-12 15:33:21 -0500

    recursos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AutorModule } from './autor/autor.module';
+import { PlaylistModule } from './playlist/playlist.module';
+import { CancionModule } from './cancion/cancion.module';
+import { GeneroModule } from './genero/genero.module';
+import { AutorModule } from './autor/autor.module';
 
 @Module({
-  imports: [],
+  imports: [AutorModule, GeneroModule, CancionModule, PlaylistModule],
   controllers: [AppController],
   providers: [AppService],
 })
